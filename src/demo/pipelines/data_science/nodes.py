ac5f9c0f8bfe2f11cdb51613add908fb4f7281d6
juan-carlos-calvo <juancarlos.calvo@quantumblack.com>
2021-01-15 10:07:29 -0300

    working demo

--- src/demo/pipelines/data_science/nodes.py
+++ src/demo/pipelines/data_science/nodes.py
@@ -39,11 +39,11 @@ from sklearn.model_selection import train_test_split
 def split_data(data: pd.DataFrame, parameters: Dict) -> List:
     """Splits data into training and test sets.
 
-        Args:
-            data: Source data.
-            parameters: Parameters defined in parameters.yml.
-        Returns:
-            A list containing split data.
+    Args:
+        data: Source data.
+        parameters: Parameters defined in parameters.yml.
+    Returns:
+        A list containing split data.
 
     """
     X = data[
@@ -66,12 +66,12 @@ def split_data(data: pd.DataFrame, parameters: Dict) -> List:
 def train_model(X_train: np.ndarray, y_train: np.ndarray) -> LinearRegression:
     """Train the linear regression model.
 
-        Args:
-            X_train: Training data of independent features.
-            y_train: Training data for price.
+    Args:
+        X_train: Training data of independent features.
+        y_train: Training data for price.
 
-        Returns:
-            Trained model.
+    Returns:
+        Trained model.
 
     """
     regressor = LinearRegression()
@@ -79,16 +79,19 @@ def train_model(X_train: np.ndarray, y_train: np.ndarray) -> LinearRegression:
     return regressor
 
 
-def evaluate_model(regressor: LinearRegression, X_test: np.ndarray, y_test: np.ndarray):
+def evaluate_model(
+    regressor: LinearRegression, X_test: np.ndarray, y_test: np.ndarray
+) -> Dict[str, float]:
     """Calculate the coefficient of determination and log the result.
 
-        Args:
-            regressor: Trained model.
-            X_test: Testing data of independent features.
-            y_test: Testing data for price.
+    Args:
+        regressor: Trained model.
+        X_test: Testing data of independent features.
+        y_test: Testing data for price.
 
     """
     y_pred = regressor.predict(X_test)
     score = r2_score(y_test, y_pred)
     logger = logging.getLogger(__name__)
     logger.info("Model has a coefficient R^2 of %.3f.", score)
+    return {"r2": score}
--- src/demo/run.py
+++ src/demo/run.py
@@ -35,7 +35,7 @@ from kedro.framework.session import KedroSession
 def run_package():
     # Entry point for running a Kedro project packaged with `kedro package`
     # using `python -m <project_package>.run` command.
-    package_name = Path(__file__).resolve().parent.name
+    package_name = str(Path(__file__).resolve().parent.name)
     with KedroSession.create(package_name) as session:
         session.run()
 
